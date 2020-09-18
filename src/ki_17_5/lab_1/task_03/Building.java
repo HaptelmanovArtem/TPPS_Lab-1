a5ff57a31169a02c952891e968d57e5dad32afea
Dima <trluxsus@gmail.com>
2020-09-18 14:50:03 +0300

    TPPS_Lab-1.
    Building class add getters and setters for fields.

--- src/ki_17_5/lab_1/task_03/Building.java
+++ src/ki_17_5/lab_1/task_03/Building.java
@@ -4,4 +4,28 @@ public class Building {
     private String streetName;
     private Integer floorsNumber;
     private Integer buildingNumber;
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public Integer getFloorsNumber() {
+        return floorsNumber;
+    }
+
+    public void setFloorsNumber(Integer floorsNumber) {
+        this.floorsNumber = floorsNumber;
+    }
+
+    public Integer getBuildingNumber() {
+        return buildingNumber;
+    }
+
+    public void setBuildingNumber(Integer buildingNumber) {
+        this.buildingNumber = buildingNumber;
+    }
 }
