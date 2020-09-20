40755364d5c29f30a51d30205d4eba73099aba73
Artem <40041781+HaptelmanovArtem@users.noreply.github.com>
2020-09-20 13:27:17 +0300

    Update Building.java

--- src/ki_17_5/lab_1/task_03/Building.java
+++ src/ki_17_5/lab_1/task_03/Building.java
@@ -1,9 +1,9 @@
 package ki_17_5.lab_1.task_03;
 
 public class Building {
-    private String streetName;
-    private Integer floorsNumber;
-    private Integer buildingNumber;
+    private readonly String streetName;
+    private readonly Integer floorsNumber;
+    private readonly Integer buildingNumber;
 
     public String getStreetName() {
         return streetName;
